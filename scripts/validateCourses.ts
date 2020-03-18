20ec39fa7dfe3065a0b89b982421f46dcf4da440
Ali Waseem <github@aliwaseem.com>
2020-03-17 20:03:57 -0600

    added simple script to run all course files

--- scripts/validateCourses.ts
+++ scripts/validateCourses.ts
@@ -1,18 +1,33 @@
 import Avj from "ajv";
 import JSONSchema from "../JSONSchema/schema.json";
-import CourseTest from "../data/subjects/javascript/courses/numbers.json";
+import glob from "glob";
+import { promisify } from "util";
 
-try {
-  const ajv = new Avj();
+const globPromise = promisify(glob);
 
-  const validate = ajv.compile(JSONSchema);
-  const valid = validate(CourseTest);
+async function main() {
+  try {
+    const ajv = new Avj();
+    const validate = ajv.compile(JSONSchema);
 
-  if (!valid) {
-    console.error(validate.errors);
+    const files: string[] = await globPromise(
+      `${__dirname}/../data/subjects/**/courses/*.json`
+    );
+
+    const JSONCourses = await Promise.all(files.map(file => import(file)));
+
+    JSONCourses.forEach(JSON => {
+      const valid = validate(JSON.default);
+
+      if (!valid) {
+        console.error(validate.errors);
+        process.exit(1);
+      }
+    });
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
-} catch (error) {
-  console.error(error);
-  process.exit(1);
 }
+
+main();
